e12a455e28ad45193f67ff4ea1ad9de63ac30551
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2022-02-18 13:24:57 +0100

    Use new boost bind placeholders

--- industrial_robot_client/src/joint_trajectory_action.cpp
+++ industrial_robot_client/src/joint_trajectory_action.cpp
@@ -43,8 +43,8 @@ const double JointTrajectoryAction::WATCHDOG_PERIOD_ = 1.0;
 const double JointTrajectoryAction::DEFAULT_GOAL_THRESHOLD_ = 0.01;
 
 JointTrajectoryAction::JointTrajectoryAction() :
-    action_server_(node_, "joint_trajectory_action", boost::bind(&JointTrajectoryAction::goalCB, this, _1),
-                   boost::bind(&JointTrajectoryAction::cancelCB, this, _1), false), has_active_goal_(false),
+    action_server_(node_, "joint_trajectory_action", boost::bind(&JointTrajectoryAction::goalCB, this, boost::placeholders::_1),
+                   boost::bind(&JointTrajectoryAction::cancelCB, this, boost::placeholders::_1), false), has_active_goal_(false),
                    controller_alive_(false), has_moved_once_(false), name_("joint_trajectory_action")
 {
   ros::NodeHandle pn("~");
